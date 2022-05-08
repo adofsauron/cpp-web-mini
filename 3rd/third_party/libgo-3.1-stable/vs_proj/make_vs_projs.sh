51858cf9e94bdb85185b3a10f5d728fab61df9e5
adofsauron <adofsauron@163.com>
2022-05-08 16:59:26 +0800

    fix master

--- 3rd/third_party/libgo-3.1-stable/vs_proj/make_vs_projs.sh
+++ 3rd/third_party/libgo-3.1-stable/vs_proj/make_vs_projs.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-mkdir vs2017/x64 vs2015/x86 vs2015/x64 -p
+mkdir vs2019/x64 vs2015/x86 vs2015/x64 -p
 
 PATH=$PATH:/c/Program\ Files/CMake/bin/
 
-rm vs2017/x64/* -rf
-cd vs2017/x64
-cmake.exe ../../.. -G"Visual Studio 15 2017 Win64" $@
+rm vs2019/x64/* -rf
+cd vs2019/x64
+cmake.exe ../../.. -G"Visual Studio 15 2019 Win64" $@
 cd ../..
 
 #rm vs2015/x86/* -rf
